30d727dc28e6f286a11e02f74f2bff2cf6fe42c6
student3 <student3@DESKTOP-IVC8MIU>
2022-03-10 09:38:44 +0545

    Add project files.

---
+++ asp.netproject/Controllers/employeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp.netproject.Models;
+
+namespace asp.netproject.Controllers
+{
+    public class employeeController : Controller
+    {
+        MainEntities db;
+        public employeeController()
+        {
+            db = new MainEntities();
+        }
+        // GET: employee
+        public ActionResult Employee()
+        {
+            List<asptable>all_data = db.asptables.ToList();
+            return View(all_data);
+        }
+    }
+}
---
+++ asp.netproject/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("asp.netproject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("asp.netproject")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1073a1a-96ae-4957-8b00-b6c8db42af8a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
