1342411f91be038454239470d614173dd660b72b
anupzpt <prajapatianu433@gmail.com>
2022-04-03 15:18:34 +0545

    edited and deleted

--- asp.netproject/Controllers/RecordController.cs
+++ asp.netproject/Controllers/RecordController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,7 +21,19 @@ namespace asp.netproject.Controllers
             List<record> all_data = recdb.records.ToList();
             return View(all_data);
         }
-       
+        public ActionResult Edit(int id)
+        {
+            student data = recdb.students.Find(id);
+            //column ko anusar select garna
+            //record data =db.records.FirstOrDefault(x => x.id == id);
+            return View(data);
+        }
+        public ActionResult UpdateData(student update)
+        {
+            recdb.Entry(update).State = EntityState.Modified;
+            recdb.SaveChanges();
+            return RedirectToAction("record");
+        }
         public ActionResult Savedata(record recordsave)
         {
             recdb.records.Add(recordsave);
@@ -41,6 +54,19 @@ namespace asp.netproject.Controllers
         public ActionResult adddata(student studentsave)
         {
             recdb.students.Add(studentsave);
+            recdb.SaveChanges();
+            return RedirectToAction("student");
+        }
+        public ActionResult Delete(int id)
+        {
+            student data = recdb.students.Find(id);
+            return View(data);
+        }
+        public ActionResult Deletedata(int id)
+        {
+            student record = recdb.students.Find(id);
+
+            recdb.students.Remove(record);
             recdb.SaveChanges();
             return RedirectToAction("student");
         }
